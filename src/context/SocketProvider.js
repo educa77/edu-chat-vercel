bd8aaa87bc495ed31dd292c7e34abdafcc1692aa
educalo77 <educalo@outlook.com>
2021-01-21 10:08:53 -0300

    ok

--- src/context/SocketProvider.js
+++ src/context/SocketProvider.js
@@ -12,8 +12,8 @@ export function SocketProvider({ id, children }) {
 
     const [socket, setSocket] = useState()
 
-    const HOST = "https://edu-chat-heroku.herokuapp.com/socket.io/?transport=polling";
-
+ //   const HOST = "https://edu-chat-heroku.herokuapp.com/socket.io/?transport=polling";
+const HOST = "ws://edu-chat-heroku.herokuapp.com:3037/socket.io/?EIO=4&transport=websocket"
     useEffect(() => {
         const newSocket = io(
              HOST,
