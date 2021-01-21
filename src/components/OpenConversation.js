aed05024a61b4bc02ad32678c029ea4ba0b79f2f
educalo77 <educalo@outlook.com>
2021-01-21 14:59:11 -0300

    ok

--- src/components/OpenConversation.js
+++ src/components/OpenConversation.js
@@ -26,14 +26,14 @@ export default function OpenConversation() {
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
-                <div className="d-flex flex-column align-items-start justify-content-end px-3 w-10 ">
+                <div className="d-flex flex-column align-items-start justify-content-end px-3 ">
                     {selectedConversation.messages.map((message, index) => {
                         const lastMessage = selectedConversation.messages.length - 1 === index;
                         return (
                             <div
                                 ref={lastMessage ? setRef : null}
                                 key={index}
-                                className={`my-1 d-flex flex-column ${message.fromMe ? `align-self-end align-items-end` : `align-items-start`}`}
+                                className={`my-1 d-flex flex-column mw-50 ${message.fromMe ? `align-self-end align-items-end` : `align-items-start`}`}
                             >
                                 <div className={`rounded px-2 py-1 ${message.fromMe ? `bg-primary text-white` : `border`}`} >
                                     {message.text}
